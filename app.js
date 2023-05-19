39b58d249860fb2f7b30c89838c5281ebfae6c03
Fran-JF <franciscouzcategui2021@gmail.com>
2023-05-19 13:32:22 -0400

    Clases para generar el producto y las funciones

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
-//! Configuracion (icono desplegable)
+//* Configuracion (icono desplegable)
 document.querySelector('.select-arrow').addEventListener('click', function() {
 document.querySelector('#opciones').click();
 });
+
+//*Construccion del Producto dentro de la app
+class Product{
+    constructor(nombre, descripcion, categoria, precio, año){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.año = año;        
+    }
+}
+
+//*Construccion de los Metodos (Funcionalodades)
+
+class Metodo {
+    // Añadir un producto
+    añadirPro(){
+
+    }
+
+    // Eliminar un producto
+    eliminarPro(){
+
+    }
+
+    // Mensajes para mantener informado al usuario
+    mostrarMsg(){
+
+    }
+}
+
+
+//* Eventos del DOM (Interactividad del ussuario)
+
+//Envio de los datos de Formulario
